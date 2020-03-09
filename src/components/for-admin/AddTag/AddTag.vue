0df6eb042a036f48cb1cb7eeead34fed752a7443
YulyKo <juliakotenko.forstudy@gmail.com>
2020-03-10 00:25:26 +0200

    fix

--- src/components/for-admin/AddTag/AddTag.vue
+++ src/components/for-admin/AddTag/AddTag.vue
@@ -1,9 +1,10 @@
 <template>
   <main class="flex AddingTag">
     <form class="form" id="form" @submit="addTag">
+      <p>{{ this.error }}</p>
       <label for="type-tag">Type of tag</label>
       <select id="type-tag" v-model="tagType">
-        <option v-for="tagType of tagsTypes" :value="tagType.url" :key="tagType.id">{{tagType.name}}</option>
+        <option v-for="tagType of tagsTypes" :value="tagType.url" :key="tagType.id" :max="15">{{ tagType.name }}</option>
       </select>
       <label for="name">Name tag</label>
       <input v-model="tag.name" id="name" type="text">
@@ -11,7 +12,7 @@
       <input v-model="tag.bg_color" id="bgColor" type="color" pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$">
       <label for="textColor">Text color tag</label>
       <input v-model="tag.text_color" id="textColor" type="color" pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$">
-      <span class="tag" :class="stylesTag">{{tag.name}}</span>
+      <span class="tag-preview" v-bind:style="{backgroundColor: this.tag.bg_color, color: this.tag.text_color}">{{ tag.name }}</span>
       <button @click="cleanForm">Add tag</button>
     </form>
   </main>
@@ -24,18 +25,13 @@
         props: {},
         data() {
             return {
-                tagType: 'location_tags',
+                tagType: 'plot_tags',
                 tag: {
-                    name: null,
+                    name: 'name',
                     bg_color: '#CCCCCC',
                     text_color: '#333333',
                 },
                 tagsTypes: [
-                    {
-                        id: 0,
-                        name: "location",
-                        url: "location_tags"
-                    },
                     {
                         id: 1,
                         name: "plot",
@@ -52,17 +48,19 @@
                         url: "relationship_tags",
                     },
                 ],
-                stylesTag: {
-
-                },
+                error: '',
+                // stylesTag: {
+                //     backgroundColor: this.tag.bg_color
+                // },
             }
         },
         methods: {
             addTag() {
-                if (this.tag.name) {
+                if (this.tag.name.length >= 3 && this.tag.name.length <= 15) {
                     this.$store.dispatch(`http_tag_module/SET_TAG_TO_API`,
                         {payload: this.tag, url: this.tagType});
-                }
+                } else if(this.tag.name.length < 3) this.error = 'name is so shot';
+                else if (this.tag.name.length > 15) this.error = 'name is so long';
             },
             cleanForm() {
                 document.getElementById('form').reset();
