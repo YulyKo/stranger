3b29fce16dffeb45c5bed63ed9a3b6aee8798fb6
YulyKo <juliakotenko.forstudy@gmail.com>
2020-03-17 14:39:45 +0200

    add pages for hero and heroes

---
+++ src/pages/common/HeroesPage/HeroPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ hero.name }}</h1>
+    <h2>{{ hero.shot_description }}</h2>
+    <p>Age: {{ hero.age }}</p>
+    <p>{{ hero.sex }}</p>
+    <p>{{ hero.story_of_life }}</p>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from "vuex";
+
+  export default {
+    props: {},
+    computed: {
+      ...mapGetters({
+          hero_from_store: 'heroes_module/HERO'
+      }),
+      hero() {
+        return this.hero_from_store[0];
+      },
+    },
+    beforeCreate() {
+      this.$store.dispatch('heroes_module/GET_HERO_FROM_API_BY_ID', this.$route.params.id);
+    },
+  };
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/common/HeroesPage/HeroesPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <article v-for="(hero, id) in heroes" :key="id">
+      <img src="" alt="hero art">
+      <router-link tag="h2" :to="{name: 'Hero', params: {id: hero.id}}">{{ hero.name }}</router-link>
+    </article>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  props: {},
+  computed: {
+      ...mapGetters({
+          heroes: 'heroes_module/HEROES'
+      }),
+  },
+  beforeCreate() {
+      this.$store.dispatch('heroes_module/GET_HEROES_FROM_API');
+  }
+}
+</script>
+
+<style>
+
+</style>
