8c42e436842b9f132ca961ca251038e58f08a04e
YulyKo <juliakotenko.forstudy@gmail.com>
2020-03-27 14:30:24 +0200

    added to send in server

--- src/components/for-admin/AddingArt/AddingArt.vue
+++ src/components/for-admin/AddingArt/AddingArt.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="AddingArt">
-<!--    <label for="title">Title</label>-->
-<!--    <input id="title" type="text" v-model="art.title">-->
-<!--    <label for="description">Description</label>-->
-<!--    <input id="description" type="text" v-model="art.description">-->
-<!--    <div class="flex">-->
-<!--      <label class="ml-2em" v-for="(tag, id) in tags" :key="id">-->
-<!--        <input type="checkbox" :value="tag.name">-->
-<!--        {{tag.name}}-->
-<!--      </label>-->
-<!--    </div>-->
-    <div>
-      <p>Progress: {{previewImage.uploadValue.toFixed()+"%"}}
-        <progress id="progress" :value="previewImage.uploadValue" max="100" ></progress>  </p>
+    <label for="title">Title</label>
+    <input id="title" type="text" v-model="art.title">
+    <label for="description">Description</label>
+    <input id="description" type="text" v-model="art.description">
+    <div class="flex">
+      <label class="ml-2em" v-for="(tag, id) in tags" :key="id">
+        <input type="checkbox" :value="tag.name">
+        {{tag.name}}
+      </label>
     </div>
+    <span>Progress: {{previewImage.uploadValue.toFixed()+"%"}}
+      <progress id="progress" :value="previewImage.uploadValue" max="100" ></progress>
+    </span>
     <div v-if="previewImage.imageData!=null">
       <img alt="" class="preview" :src="previewImage.picture">
     </div>
@@ -31,38 +30,35 @@
       props: {},
       data() {
           return {
-              art: {
-                  author: null,
-                  title: null,
-                  description: null,
-                  url: null,
-              },
-              previewImage: {
-                  uploadValue: 0,
-                  picture: null,
-                  imageData: null,
-              },
+            art: {
+              author: null,
+              title: null,
+              description: null,
+              url: null,
+            },
+            previewImage: {
+              uploadValue: 0,
+              picture: null,
+              imageData: null,
+            },
           }
       },
       computed: {
-          ...mapGetters({
-              tags: 'tags/TAGS',
-              user: 'user/USER',
-          })
+        ...mapGetters({
+          tags: 'tags/TAGS',
+          user: 'user/USER',
+        })
       },
       methods: {
         addArt() {
-            this.uploadImage();
-          // this.image = event.target.files[0];
-          // console.log(this.image);
-          // this.art.author = this.user.login;
-          // if (this.art.title && this.art.description && this.art.author)
-          //   this.$store.dispatch('art/SET_ART_TO_API', this.art)
+          this.uploadImage();
+          this.art.author = this.user.login;
+          if (this.art.title && this.art.description && this.art.author)
+            this.$store.dispatch('art/SET_ART_TO_API', this.art);
         },
         setFile(event) {
           console.log(event.target.files[0]);
           this.previewImage.imageData = event.target.files[0];
-          // this.previewImage.uploadValue = 0;
         },
         uploadImage() {
           const storageRef = firebase.storage().ref(`arts/${this.previewImage.imageData.name}`).put(this.previewImage.imageData);
@@ -73,17 +69,14 @@
               this.previewImage.uploadValue = 100;
               storageRef.snapshot.ref.getDownloadURL().then( url => {
                 this.previewImage.picture = url;
+                this.art.url = url;
               });
             }
           )
         },
       },
-        beforeCreate() {
-          this.$store.dispatch('tags/GET_TAGS_FROM_API', 'art_tag');
-        },
-
-        // onUpload() {
-        //     const storeRef = firebase.storage().ref(`${this.image.name}`
-        // },
+      beforeCreate() {
+        this.$store.dispatch('tags/GET_TAGS_FROM_API', 'art_tag');
+      },
     };
 </script>
