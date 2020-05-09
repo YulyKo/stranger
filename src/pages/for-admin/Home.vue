fbf85c720984c5c4ee729c4848bf03172df0a745
YulyKo <juliakotenko.forstudy@gmail.com>
2020-05-09 20:45:46 +0300

    delete link to delete component

--- src/pages/for-admin/Home.vue
+++ src/pages/for-admin/Home.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="nav">
     <nav class="admin-menu">
-      <div class="admin-menu">
-        <router-link class="link" tag="a" to="/admin/add-plot">Add plot</router-link>
-        <router-link class="link" tag="a" to="/admin/add-art">Add art</router-link>
-        <router-link class="link" tag="a" to="/admin/add-hero">Add hero</router-link>
-        <router-link class="link" tag="a" to="/admin/add-location">Add location</router-link>
-        <router-link class="link" tag="a" to="/admin/add-relationship">Add relationship</router-link>
-        <div class="link"><router-link class="link" tag="a" to="/admin/add-tag">Add tag</router-link></div>
-      </div>
-      <div class="admin-menu">
-        <router-link class="link" tag="a" to="/admin/delete-plot">Delete plot</router-link>
-      </div>
+      <router-link class="link" tag="a" to="/admin/add-plot">Add plot</router-link>
+      <router-link class="link" tag="a" to="/admin/add-art">Add art</router-link>
+      <router-link class="link" tag="a" to="/admin/add-hero">Add hero</router-link>
+      <router-link class="link" tag="a" to="/admin/add-location">Add location</router-link>
+      <router-link class="link" tag="a" to="/admin/add-relationship">Add relationship</router-link>
+      <div class="link"><router-link class="link" tag="a" to="/admin/add-tag">Add tag</router-link></div>
     </nav>
     <router-view></router-view>
   </div>
