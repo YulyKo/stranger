b959789963650c42a3d4a5a496add4c4e58f13e7
YulyKo <juliakotenko.forstudy@gmail.com>
2020-09-21 11:04:49 -0700

    added styles to ArtPage.vue

--- src/pages/common/Arts/ArtPage.vue
+++ src/pages/common/Arts/ArtPage.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
-    <h1>Art</h1>
-    <article>
-      <p>{{ art.author }}</p>
-      <img :src="art.url" alt="error">
-      <h2>{{ art.title }}</h2>
-      <p>{{ art.description }}</p>
-      <div class="flex">Tags <br>
-      <span class="text text-s tag" v-for="tag in art.tags" :key="tag.id"
+    <article class="art">
+      <h2 class="text text-xl">{{ art.title }}</h2>
+      <p class="text text-m text--author">{{ art.author }}</p>
+      <img class="art__img" :src="art.url" alt="image">
+      <p class="text text-m">{{ art.description }}</p>
+      <div class="ta-center mt-2rem">Tags<br/>
+      <span class="text text-s tag mt-2rem" v-for="tag in art.tags" :key="tag.id"
       v-bind:style="{ backgroundColor: tag.bg_color, color: tag.text_color }">{{ tag.name }}</span>
     </div>
     </article>
@@ -27,3 +26,19 @@
     }
   }
 </script>
+<style lang="sass" scoped>
+@import '../../../main'
+.art
+  background-color: $page-yellow
+  &__img
+    height: 36rem
+    display: block
+    margin: 0 auto
+.text--author
+  margin-top: 2.67rem
+  margin-bottom: 0
+  padding-bottom: 2.67em
+  text-align: center
+.tag
+  margin-bottom: 3.67rem
+</style>
