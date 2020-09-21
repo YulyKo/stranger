542e515b4d05231a0f5c22f8007d8819889b02cb
YulyKo <juliakotenko.forstudy@gmail.com>
2020-09-21 11:06:26 -0700

    refactored .log-s in art.js

--- src/store/modules/arts/art.js
+++ src/store/modules/arts/art.js
@@ -14,10 +14,8 @@ const module = {
   },
   actions: {
     async GET_ART_FROM_API_BY_ID(context, id) {
-      console.log(`${URL_COMMON}/arts/${id}`);
       const { data } = await Axios.get(`${URL_COMMON}/arts/${id}`);
       context.commit('SET_ART_TO_STATE', data);
-      console.log(data)
     },
     async POST_ART_TO_API(context, payload) {
       await Axios.post(`${URL_COMMON}/arts_with_tags`, payload);
