daa95d9c054bac2bb9ab2a46094b0ae5f7956f88
YulyKo <juliakotenko.forstudy@gmail.com>
2020-09-21 11:07:11 -0700

    added styles to LocationPage.vue

--- src/pages/common/LocationPage/LocationPage.vue
+++ src/pages/common/LocationPage/LocationPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <article>
-      <h1>{{ location.name }}</h1>
-      <img :src="this.location.photo_url" alt="no photo">
-      <p>{{ location.author }}</p>
+    <article class="location">
+      <h1 class="text text-xl">{{ location.name }}</h1>
+      <img class="location__img" :src="this.location.photo_url" alt="no photo">
+      <p class="text text-m text--author">{{ location.author }}</p>
     </article>
   </div>
 </template>
@@ -12,12 +12,27 @@
   import {mapGetters} from "vuex";
 
   export default {
-      name: "LocationPage",
-      computed: {
-        ...mapGetters({location: 'location/LOCATION'})
-      },
-      beforeCreate() {
-        this.$store.dispatch('location/GET_LOCATION_FROM_API_BY_ID',  this.$route.params.id)
-      }
+    name: "LocationPage",
+    computed: {
+      ...mapGetters({location: 'location/LOCATION'})
+    },
+    beforeCreate() {
+      this.$store.dispatch('location/GET_LOCATION_FROM_API_BY_ID',  this.$route.params.id)
+    }
   }
 </script>
+
+<style lang="sass" scoped>
+@import '../../../main'
+.location
+  background-color: $page-yellow
+  &__img
+    height: 36rem
+    display: block
+    margin: 0 auto
+.text--author
+  margin-top: 2.67rem
+  margin-bottom: 0
+  padding-bottom: 2.67em
+  text-align: center
+</style>
