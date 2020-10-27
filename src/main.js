ae6c8cf15f118645bb55ffceeb175bf36c999227
YulyKo <juliakotenko.forstudy@gmail.com>
2020-10-27 21:35:18 +0200

    added eslint with babel

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store';
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 import 'firebase/firestore';
-import router from './routes';
-import  normalize from "normalize.css";
+import normalize from 'normalize.css';
 import { firestorePlugin } from 'vuefire';
-import { Photoshop } from 'vue-color'
+import { Photoshop } from 'vue-color';
 import vueAxios from 'vue-axios';
-import axios from "axios";
+import axios from 'axios';
+import router from './routes';
+import store from './store';
 
 Vue.use(firestorePlugin);
-
 
 Vue.config.productionTip = true;
 // Vue.use(VueResource);
@@ -19,14 +18,14 @@ Vue.use(vueAxios, axios);
 Vue.use(normalize);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuAkDqsaJ0XhBwSMc2ZUIQVGpEqDw9D9Y",
-  authDomain: "stranger-proba.firebaseapp.com",
-  databaseURL: "https://stranger-proba.firebaseio.com",
-  projectId: "stranger-proba",
-  storageBucket: "stranger-proba.appspot.com",
-  messagingSenderId: "820743614802",
-  appId: "1:820743614802:web:4a47543ba8b22dd8558a8b",
-  measurementId: "G-W44LF10KLX"
+  apiKey: 'AIzaSyDuAkDqsaJ0XhBwSMc2ZUIQVGpEqDw9D9Y',
+  authDomain: 'stranger-proba.firebaseapp.com',
+  databaseURL: 'https://stranger-proba.firebaseio.com',
+  projectId: 'stranger-proba',
+  storageBucket: 'stranger-proba.appspot.com',
+  messagingSenderId: '820743614802',
+  appId: '1:820743614802:web:4a47543ba8b22dd8558a8b',
+  measurementId: 'G-W44LF10KLX',
 };
 
 // Initialize Firebase
@@ -37,7 +36,7 @@ new Vue({
   router,
   store,
   components: {
-      'Photoshop': Photoshop,
+    Photoshop,
   },
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
