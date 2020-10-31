049b2e9c5d874fb78d3ad6317f7282f3eb3684b6
YulyKo <juliakotenko.forstudy@gmail.com>
2020-10-31 19:03:28 +0200

    fixed AddingLocation.vue by eslint

--- src/components/for-admin/add/AddingLocation.vue
+++ src/components/for-admin/add/AddingLocation.vue
@@ -88,8 +88,9 @@ export default {
       }
     },
     setPicture(event) {
-      this.previewImage.picture = URL.createObjectURL(event.target.files[0]);
-      this.previewImage.imageData = event.target.files[0];
+      const files = event.target.files[0];
+      this.previewImage.picture = URL.createObjectURL(files);
+      this.previewImage.imageData = files;
     },
   },
   css: [],
